982e9ae0d18eb052c5eb8030cc0a58d05407855a
MargelovN <margelovn@gmail.com>
2022-01-16 20:31:20 +0900

    ADDED FILES WITH TESTNG

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -1,27 +1,25 @@
 package ru.netology.service;
 
-import org.junit.jupiter.api.Test;
+import static org.testng.Assert.*;
 
-import static org.junit.jupiter.api.Assertions.*;
-
-class CashbackHackServiceTest {
+public class CashbackHackServiceTest {
     CashbackHackService service = new CashbackHackService();
 
-    @Test
+    @org.testng.annotations.Test
     public void shouldReturnZero() {
         int actual = service.remain(0);
         int expected = 1000;
         assertEquals(actual, expected);
     }
 
-    @Test
+    @org.testng.annotations.Test
     public void shouldReturnUpperLimit() {
         int actual = service.remain(1000);
         int expected = 0;
         assertEquals(actual, expected);
     }
 
-    @Test
+    @org.testng.annotations.Test
     public void shouldReturn100For900() {
         int actual = service.remain(900);
         int expected = 100;
