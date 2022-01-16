b0caef97a93185fb65c0f4c3946deb6359c04e00
MargelovN <margelovn@gmail.com>
2022-01-16 20:55:08 +0900

    ADDED FILES WITH JUNIT4

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -1,25 +1,27 @@
 package ru.netology.service;
 
-import static org.testng.Assert.*;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
 
 public class CashbackHackServiceTest {
     CashbackHackService service = new CashbackHackService();
 
-    @org.testng.annotations.Test
+    @Test
     public void shouldReturnZero() {
         int actual = service.remain(0);
         int expected = 1000;
         assertEquals(actual, expected);
     }
 
-    @org.testng.annotations.Test
+    @Test
     public void shouldReturnUpperLimit() {
         int actual = service.remain(1000);
         int expected = 0;
         assertEquals(actual, expected);
     }
 
-    @org.testng.annotations.Test
+    @Test
     public void shouldReturn100For900() {
         int actual = service.remain(900);
         int expected = 100;
